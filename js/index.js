0ca1d62166ef7d1754bba87c984e3d5f6e20413f
Géraldine <gperez.zen@gmail.com>
2024-03-31 14:56:59 +0200

    adjust CSS projects and menu click

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@
     const app = {
         init: function() {
             app.toggleMenu();
+            app.clickItem();
             console.info('initialization');
         },
         
@@ -25,8 +26,28 @@
                     menu.classList.add("fa-bars");
                     header.classList.remove("toggle");
                 }
-            });
-        }
+            })},
+
+        
+
+        clickItem: function() {
+            const header = document.getElementById("bigMenu");
+            header.addEventListener('click', function() {    
+                const menu = document.getElementById("menu");
+
+                if (menu.style.display === "block") {
+                    menu.classList.remove("fa-times");
+                    menu.classList.add("fa-bars");
+                    header.classList.remove("toggle");
+                    menu.style.display = "none"; // Pour masquer le menu
+                                } else {
+                    menu.classList.remove("fa-bars");
+                    menu.classList.add("fa-times");
+                    header.classList.add("toggle");
+                    menu.style.display = "block"; // Pour afficher le menu
+                };
+        })}
+        
     };
 
     document.addEventListener('DOMContentLoaded', app.init);
